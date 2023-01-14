a594add38edac7c651d4d7616c8685a1f94d208d
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2023-01-14 16:43:10 -0500

    Amplifier retreat

--- src/AFirstSubmissionBot/BotAmplifier.java
+++ src/AFirstSubmissionBot/BotAmplifier.java
@@ -12,12 +12,13 @@ public class BotAmplifier extends Explore{
         ANCHOR_FOLLOWER,
     }
 
-
     private static Status amplifierState = Status.BORN;
     private static RobotInfo[] visibleEnemies;
     private static RobotInfo[] visibleAllies;
     private static int commAllyRobots = 0;
     private static int vNonHQEnemies = 0;
+    private static int vNonHQCombatEnemies = 0;
+    private static int vNonHQCombatAllies = 0;  
     private static MapLocation closestEnemyLocation;
 
     public static void initAmplifier() throws GameActionException{
@@ -28,15 +29,66 @@ public class BotAmplifier extends Explore{
         closestEnemyLocation = null;
         updateVision();
         sendCombatLocation(visibleEnemies);
-        if (vNonHQEnemies == 0 && commAllyRobots > 0){
+        if (rc.isMovementReady() && vNonHQCombatEnemies > vNonHQCombatAllies){
+            tryToBackUpToMaintainMaxRangeAmplifier();
+            if (amplifierState == Status.FLEEING){
+            }
+        } 
+        else if (vNonHQEnemies == 0 && commAllyRobots > 0){
             Direction away = directionAwayFromAmplifierAndHQ(visibleAllies);
             if (away!=null && Movement.tryMoveInDirection(explore(away))){
-                // amplifierState = Status.SPREADING;
+                amplifierState = Status.SPREADING;
             }
         }
+        updateVision();
         if (Clock.getBytecodesLeft() > 700) findAndWriteWellLocationsToComms();
         rc.setIndicatorString(amplifierState.toString() + " " + closestEnemyLocation);
     }
+
+    public static boolean tryToBackUpToMaintainMaxRangeAmplifier() throws GameActionException {
+		int closestHostileDistSq = Integer.MAX_VALUE;
+        MapLocation lCR = rc.getLocation();
+        for (int i= visibleEnemies.length; --i >= 0;) {
+            RobotInfo hostile = visibleEnemies[i];
+			if (!hostile.type.canAttack()) continue;
+			int distSq = lCR.distanceSquaredTo(hostile.location);
+			if (distSq < closestHostileDistSq) {
+				closestHostileDistSq = distSq;
+			}
+		}
+		
+		Direction bestRetreatDir = null;
+		int bestDistSq = closestHostileDistSq;
+
+		for (Direction dir : directions) {
+			if (!rc.canMove(dir)) continue;
+			MapLocation dirLoc = lCR.add(dir);
+            boolean isDirPassable = rc.sensePassability(dirLoc);
+            if (!isDirPassable) continue; // Don't try to move to impassable location
+
+			int smallestDistSq = Integer.MAX_VALUE;
+			for (int i= visibleEnemies.length; --i >= 0;) {
+                RobotInfo hostile = visibleEnemies[i];
+				if (!hostile.type.canAttack()) continue;
+				int distSq = hostile.location.distanceSquaredTo(dirLoc);
+				if (distSq < smallestDistSq) {
+					smallestDistSq = distSq;
+				}
+			}
+			if (smallestDistSq > bestDistSq) {
+				bestDistSq = smallestDistSq;
+				bestRetreatDir = dir;
+			}
+		}
+		if (bestRetreatDir != null) {
+			rc.move(bestRetreatDir);
+            currentLocation = rc.getLocation();
+			explore(bestRetreatDir);
+            amplifierState = Status.FLEEING;
+			return true;
+		}
+		return false;
+	}
 
     private static Direction directionAwayFromAmplifierAndHQ(RobotInfo[] givenRobots){
         MapLocation currentTarget = rc.getLocation();
@@ -66,18 +118,22 @@ public class BotAmplifier extends Explore{
     private static void updateVision() throws GameActionException {
         commAllyRobots = 0;
         visibleEnemies = rc.senseNearbyRobots(UNIT_TYPE.visionRadiusSquared, ENEMY_TEAM);
-        // inRangeEnemies = rc.senseNearbyRobots(UNIT_TYPE.actionRadiusSquared, ENEMY_TEAM);
         visibleAllies = rc.senseNearbyRobots(UNIT_TYPE.visionRadiusSquared, MY_TEAM);
-        // inRangeAllies = rc.senseNearbyRobots(UNIT_TYPE.actionRadiusSquared, MY_TEAM);
         vNonHQEnemies = 0;
         for (int i = visibleEnemies.length; --i >= 0;) {
             if (visibleEnemies[i].type != RobotType.HEADQUARTERS) {
                 vNonHQEnemies++;
+                if (visibleEnemies[i].type == RobotType.LAUNCHER){
+                    vNonHQCombatEnemies++;
+                }
             }
         }
         for (int i = visibleAllies.length; --i >= 0;) {
             if (visibleAllies[i].type == RobotType.HEADQUARTERS || visibleAllies[i].type == RobotType.AMPLIFIER) {
                 commAllyRobots++;
+            }
+            else if (visibleAllies[i].type == RobotType.LAUNCHER){
+                vNonHQCombatAllies++;
             }
         }
     }
