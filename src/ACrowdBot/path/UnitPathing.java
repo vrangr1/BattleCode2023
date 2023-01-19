8549f8ead24b10b9beb56d3fa5df388aad0f22e9
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2023-01-19 00:13:27 -0500

    Introducing ACrowdBot

---
+++ src/ACrowdBot/path/UnitPathing.java
@@ -0,0 +1,7 @@
+package ACrowdBot.path;
+
+import battlecode.common.*;
+
+public interface UnitPathing {
+    public Direction bestDir(MapLocation target) throws GameActionException;
+}
--- src/APostPatchBot/path/UnitPathing.java
+++
@@ -1,7 +0,0 @@
-package APostPatchBot.path;
-
-import battlecode.common.*;
-
-public interface UnitPathing {
-    public Direction bestDir(MapLocation target) throws GameActionException;
-}
